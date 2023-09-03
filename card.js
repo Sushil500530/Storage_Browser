cab5c7ed7c2b2af1e7a15d0f1a7a71a3a2119b99
Sushil97 <sushil500530@gmail.com>
2023-09-03 17:56:07 +0600

    card set localstore and show to distplay other in time

--- card.js
+++ card.js
@@ -7,6 +7,8 @@
     quantity.value = '' ;
     console.log(productValue, quantityValue)
     displayProduct(productValue, quantityValue)
+    saveLocalStorage(productValue,quantityValue)
+
  } ;
 
 
@@ -16,5 +18,40 @@
     li.innerText = `${product} : ${quantity}` ;
     ProductContainer.appendChild(li)
  }
+// store kora data ta amra kivabe paite pari seta 
+ const getStoredShopingCard = () =>{
+    const storedCard = localStorage.getItem('card');
+    let card = {} ;
+    if(storedCard){
+        card = JSON.parse(storedCard)
+    }
+    return card ;
+ }
+// set korar jonno(ager j store kora data cilo seta ke call kore)
+ const saveLocalStorage = (product, quantity)=>{
+    const getCard = getStoredShopingCard()
+    getCard[product] = quantity ;
+    // console.log(getCard)
+    const cardStringified = JSON.stringify(getCard)
+    console.log(cardStringified)
+    localStorage.setItem('card', cardStringified)
+ }
 
-//  local Storage a rakhar jonno kaj korte hobe jeno reload dile harai na jay
+//  ===> uporer kaj gulo korar por sob gulo inputkrito data gulo localStorage a store(joma) kore rakhtece.. akhen ai data gulo jodi reload dewar poreo dekhate chai tahle nicher kaj ta kore korte pari 
+
+// item gulo akta akta kore dekhanor kaj ta korteci
+const displayProductsShowLocalStorage =()=>{
+     const showDisplay = getStoredShopingCard()
+     console.log(showDisplay)
+    //  akhn protita product er nam gulo dekhar jonno loop chalabo
+    for(const product in showDisplay){
+        const quantity = showDisplay[product] ;
+        console.log(product, quantity)
+        displayProduct(product, quantity)
+    }
+}
+displayProductsShowLocalStorage()
+
+// 1... local Storage a rakhar jonno kaj korte hobe jeno reload dile harai na jay
+
+//2... variable er moddhe kono value thakle take [] er moddhe nite hobe 
